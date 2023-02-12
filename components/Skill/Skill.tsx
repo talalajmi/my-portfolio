22d03a33243fb5d32646159b39386bbe068d9659
Talal Al Ajmi <talalajmi98@gmail.com>
2023-02-12 20:31:51 +0400

    Skill white bg hover fixed

--- components/Skill/Skill.tsx
+++ components/Skill/Skill.tsx
@@ -26,7 +26,7 @@ const Skill = ({ directionLeft, skillImg, skillMastery }: Props) => {
         src={skillImg}
         alt="skill-image"
       />
-      <div className="absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32">
+      <div className="absolute z-0 h-20 w-20 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32">
         <div className="flex h-full items-center justify-center">
           <p className="text-3xl font-bold text-black opacity-100">
             {skillMastery}%
